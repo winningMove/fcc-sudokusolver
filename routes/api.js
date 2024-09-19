35585a61c5bd95deac53c3480a3bff3351fd7225
winningMove <dariopapic+github@gmail.com>
2024-09-19 08:31:13 +0200

    add solver logic

--- routes/api.js
+++ routes/api.js
@@ -1,18 +1,11 @@
-'use strict';
+"use strict";
 
-const SudokuSolver = require('../controllers/sudoku-solver.js');
+const SudokuSolver = require("../controllers/sudoku-solver.js");
 
 module.exports = function (app) {
-  
-  let solver = new SudokuSolver();
+  const solver = new SudokuSolver();
 
-  app.route('/api/check')
-    .post((req, res) => {
+  app.route("/api/check").post((req, res) => {});
 
-    });
-    
-  app.route('/api/solve')
-    .post((req, res) => {
-
-    });
+  app.route("/api/solve").post((req, res) => {});
 };
