90f156a0dad8683da87533869566edfd97369da5
winningMove <dariopapic+github@gmail.com>
2024-09-19 08:43:50 +0200

    add route handling

--- routes/api.js
+++ routes/api.js
@@ -5,7 +5,23 @@ const SudokuSolver = require("../controllers/sudoku-solver.js");
 module.exports = function (app) {
   const solver = new SudokuSolver();
 
-  app.route("/api/check").post((req, res) => {});
+  app.route("/api/check").post((req, res) => {
+    const { puzzle: puzzleString, coordinate, value } = req.body;
+    if (!puzzleString || !coordinate || !value)
+      return { error: "Required field(s) missing" };
 
-  app.route("/api/solve").post((req, res) => {});
+    const checked = solver.checkPlacementInString(
+      puzzleString,
+      coordinate,
+      value
+    );
+    return checked;
+  });
+
+  app.route("/api/solve").post((req, res) => {
+    if (!req.body.puzzle) return { error: "Required field missing" };
+    const puzzleString = req.body.puzzle;
+    const solution = solver.solve(puzzleString);
+    return solution;
+  });
 };
